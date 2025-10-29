06635dbe4a7b722fcd8a35b8a21f7b7b19d8fd20
SpaceBeee <145757626+maycra01@users.noreply.github.com>
2025-10-29 22:22:30 +0000

    Update play.py

---
+++ programming/play.py
@@ -0,0 +1,11 @@
+"""
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2025/summer.php
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2025/summer.php
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2025/9618-s25-qp-42.php
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2025/9618-s25-qp-43.php
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2023/winter.php
+https://bestexamhelp.com/exam/cambridge-international-a-level/computer-science-9618/2023/9618_w23_qp_43.pdf
+PythonNotesForProfessionals
+https://www.cambridgeinternational.org/Images/636089-2024-2025-syllabus.pdf
+
+"""
